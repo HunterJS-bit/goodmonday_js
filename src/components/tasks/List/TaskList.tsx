aa547759764fb9a06b5bfb5c463f37e188d8ca7f
miljkovic89 <markomiljkovic2@gmail.com>
2024-04-22 01:26:53 +0200

    feat(tasks): minor refactor on task list

--- src/components/tasks/List/TaskList.tsx
+++ src/components/tasks/List/TaskList.tsx
@@ -1,5 +1,4 @@
 import React, { useReducer } from 'react';
-<<<<<<< HEAD
 import { taskReducer, initialState, Action } from '../../../reducers/taskReducer';
 import { taskListCrudAction } from '../../../utils/tasks';
 import { Task } from '../../../interfaces/Task';
@@ -29,41 +28,25 @@ const TaskList: React.FC<TaskListProps> = ({ tasks }) => {
         dispatch({ type: "REDO" })
     }
 
-    const onEditTask = (index: number, done: boolean, title: string) => {
-        dispatch({ type: 'EDIT_TASK', index, done, title })
+    const onEditTask = () => {
+        dispatch({ type: 'EDIT_TODO' })
+    }
+
+    const onCompleteTask = (index: number, isDone: boolean) => {
+        dispatch({ type: "SET_CHECK", index, isDone: !isDone })
     }
 
     const onDeleteTask = (index: number) => {
-        dispatch({ type: "DELETE_TASK", index })
+        dispatch({ type: "DELETE_TODO", index })
     }
 
-    const onConfirm = async () => {
-        const { pendingChanges } = state;
-        console.log('changes', pendingChanges)
-=======
-import { taskReducer, initialState } from '../../../reducers/taskReducer';
-import { getTodoItems } from '../../../services/api/services/goodMonday.service';
-import TaskItem from './item/TaskItem';
-
-
-
-
-const TaskList = () => {
-
-    const [state, dispatch] = useReducer(taskReducer, initialState);
-
-    console.log('asdasd', state)
-
-
-    const onAddTask = () => {
-        dispatch({ type: "ADD_TODO", text: 'as' });
->>>>>>> 175d273... feat(tasks): add base crud oprations for task list
+    const onConfirm = () => {
+        console.log(state?.tasks)
     }
 
     return (
         <>
             <div className="mt-8 mb-3">
-<<<<<<< HEAD
                 <Button
                     className={`border-2 border-red-500 text-red-500 ${!state.prevState && 'cursor-not-allowed opacity-50'}`}
                     onClick={onUndo}
@@ -103,45 +86,10 @@ const TaskList = () => {
                     <span>Confirm Changes</span>
                 </Button>
             </div>
-=======
-                <button
-                    className="border-2 border-red-500 p-2 text-red-500"
-                    onClick={() => dispatch({ type: "UNDO" })}
-                    disabled={!state.prevState}
-                >      ← Undo</button>
-                <button
-                    className="border-2 border-indigo-500 p-2 text-indigo-500 ml-4"
-                    onClick={() => dispatch({ type: "REDO" })}
-                    disabled={!state.nextState}
-                > Redo →</button>
-            </div>
-            <button
-                className="border-2 border-green-500 p-2 text-green-500 hover:text-white hover:bg-green-500 rounded-lg flex"
-                onClick={onAddTask}
-            >
-                <svg className="h-6 w-6" width="24" height="24" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" fill="none" strokeLinecap="round" strokeLinejoin="round">  <path stroke="none" d="M0 0h24v24H0z" />  <circle cx="12" cy="12" r="9" />  <line x1="9" y1="12" x2="15" y2="12" />  <line x1="12" y1="9" x2="12" y2="15" /></svg>
-                <span>Add</span>
-            </button>
-            <div className='pt-5'>
-                {state.tasks.map((todo, i) => (
-                    <TaskItem
-                        key={i}
-                        text={todo.text}
-                        isCompleted={todo.isDone}
-                        onCheck={() => dispatch({ type: "SET_CHECK", i, isDone: !todo.isDone })}
-                        onRemove={() => dispatch({ type: "DELETE_TODO", i })}
-                    />
-                ))}
-            </div>
->>>>>>> 175d273... feat(tasks): add base crud oprations for task list
         </>
 
     )
 
 };
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 175d273... feat(tasks): add base crud oprations for task list
 export default TaskList;
--- src/components/tasks/List/item/TaskItem.tsx
+++ src/components/tasks/List/item/TaskItem.tsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-<<<<<<< HEAD
 import Button from '../../../common/Buttons/Button';
 import Input from '../../../common/inputs/Input';
 
@@ -44,25 +43,6 @@ const TaskItem: React.FC<TaskItemProps> = ({ text, isCompleted, onCheck, onRemov
             <span>Remove</span>
         </Button>}
     </div >)
-=======
-
-
-const TaskItem = ({ text, index, isCompleted, onCheck, onRemove, onEdit }) => {
-
-    const [isEdit, setIsEdit] = useState(false);
-
-
-    return (<div className="flex mb-4 items-center">
-        {!isEdit && <p className={`w-full text-grey-darkest ${isCompleted && 'line-through'} `} onClick={onCheck}>{text}</p>}
-        {isEdit && <input value={text} />}
-        <button className="flex-no-shrink p-2 ml-4 mr-2 border-2 rounded hover:text-white text-green border-green hover:bg-green" onClick={() => setIsEdit(!isEdit)} >Edit</button>
-        <button
-            className="flex text-red-500 border-2 border-red-500 p-2 rounded-lg" onClick={onRemove}>
-            <svg className="h-6 w-6 text-red-500" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" strokeLinecap="round" strokeLinejoin="round">  <circle cx="12" cy="12" r="10" />  <line x1="15" y1="9" x2="9" y2="15" />  <line x1="9" y1="9" x2="15" y2="15" /></svg>
-            <span>Remove</span>
-        </button>
-    </div>)
->>>>>>> 175d273... feat(tasks): add base crud oprations for task list
-};
+}
 
 export default TaskItem;
