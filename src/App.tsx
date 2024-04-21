1379209e8397d1e7d88ada9f0a719f0dd0983569
miljkovic89 <markomiljkovic2@gmail.com>
2024-04-21 12:44:45 +0200

    feat: added base router setup
    - added main routes
    - update folder structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { HashRouter, Route, Routes } from 'react-router-dom';
-import Home from './pages/Home';
-import NotFound from './pages/NotFound';
+import {
+  RouterProvider,
+} from "react-router-dom";
+import router from './router';
 
-export function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-    </Routes>
-  );
-}
+const App = () => {
+  return (<RouterProvider router={router} />)
+};
 
-export function WrappedApp() {
-  return (
-    <HashRouter>
-      <App />
-    </HashRouter>
-  );
-}
+export default App;
