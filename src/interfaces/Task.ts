4e41f2ff11e3d366633b3207ca784ad773f7f2fd
miljkovic89 <markomiljkovic2@gmail.com>
2024-04-23 00:14:21 +0200

    chore(tasks): add ts definitions for common cmps

--- src/interfaces/Task.ts
+++ src/interfaces/Task.ts
@@ -1,21 +1,12 @@
 export interface Task {
     id?: string;
-<<<<<<< HEAD
     title: string;
     done: boolean;
-=======
-    text: string;
-    isDone: boolean;
->>>>>>> 77742ed... chore(tasks): prettier format
 }
 
 export interface TaskCreatePayload {
     id?: string;
-<<<<<<< HEAD
     title: string;
-=======
-    text: string;
->>>>>>> 77742ed... chore(tasks): prettier format
     done: boolean;
 }
 
--- src/utils/tasks/index.ts
+++ src/utils/tasks/index.ts
@@ -1,31 +1,18 @@
 import { addTask, updateTask, removeTask } from "../../services/api/services/goodMonday.service";
 
 
-const taskListCrudAction = async (taskList) => {
-<<<<<<< HEAD
-    const ids = [];
-    for (task in taskList)
-    {
-        if (item.type === "ADD_TASK")
-        {
-            const addedTask = await addTask(item.task);
-            ids.push(addedTask);
-        }
-    }
-=======
-    const taskPromises = await Promise.all(taskList.map(async (action) => {
-        if (action.type === 'ADD_TODO') {
-            return addTask(action.task);
-        }
-        if (action.type === 'EDIT_TODO') {
-            return updateTask(action.task.id, action.task);
-        }
-        // if (action.type === 'EDIT_TODO') {
-        //     updateTask();
-        // }
-    }))
-    return taskPromises;
->>>>>>> 77742ed... chore(tasks): prettier format
+const taskListCrudAction = async (taskList: any) => {
+    // const taskPromises = await Promise.all(taskList.map(async (action) => {
+    //     if (action.type === 'ADD_TODO') {
+    //         return addTask(action.task);
+    //     }
+    //     if (action.type === 'EDIT_TODO') {
+    //         return updateTask(action.task.id, action.task);
+    //     }
+    //     if (action.type === 'DELETE_TODO') {
+    //         return removeTask(action.task.id);
+    //     }))
+    // return taskPromises;
 }
 
 
