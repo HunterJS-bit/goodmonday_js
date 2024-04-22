847eba37506b5a0abbd3cf4af0af2278c307dc90
miljkovic89 <markomiljkovic2@gmail.com>
2024-04-23 01:01:10 +0200

    style(tasks):  update main page layout

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
 import LoginForm from '../components/forms/LoginForm';
-<<<<<<< HEAD
 import Page from '../layout/PageLayout';
-=======
-import Page from '../layout/PageLayou';
->>>>>>> 175d273... feat(tasks): add base crud oprations for task list
 
 
 const LoginPage = () => {
