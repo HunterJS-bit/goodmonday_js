f3043babb28b5e67f1424a90723b6160f987629d
miljkovic89 <markomiljkovic2@gmail.com>
2024-04-23 20:20:53 +0200

    fix(tasks): minor refactor task reducer

--- src/utils/tasks/index.ts
+++ src/utils/tasks/index.ts
@@ -1,17 +1,23 @@
 import { addTask, updateTask, removeTask } from '../../services/api/services/goodMonday.service';
 
 const taskListCrudAction = async (taskList: any) => {
-  // const taskPromises = await Promise.all(taskList.map(async (action) => {
-  //     if (action.type === 'ADD_TODO') {
-  //         return addTask(action.task);
-  //     }
-  //     if (action.type === 'EDIT_TODO') {
-  //         return updateTask(action.task.id, action.task);
-  //     }
-  //     if (action.type === 'DELETE_TODO') {
-  //         return removeTask(action.task.id);
-  //     }))
-  // return taskPromises;
+  for (let item of taskList)
+  {
+    if (item?.type === 'EDIT')
+    {
+      if (!item.id)
+      {
+        await addTask(item?.task);
+      } else
+      {
+        await updateTask(item.id, item.task);
+      }
+    }
+    if (item?.type === 'DELETE')
+    {
+      await removeTask(item.id);
+    }
+  }
 };
 
 export { taskListCrudAction };
